0fe96c7e806e32bc55c295513ca723dcc5cdfcc2
Negar Razazzadeh <negarrazz1380@gmail.com>
2026-07-16 15:33:11 -0400

    Fix image_url null type error

--- app/product/[id]/ProductPageClient.tsx
+++ app/product/[id]/ProductPageClient.tsx
@@ -138,7 +138,7 @@ export function ProductPageClient({ product }: ProductPageClientProps) {
                       id: product.id,
                       name: product.name,
                       price: product.price,
-                      image_url: product.image_url,
+                      image_url: product.image_url ?? undefined,
                       device_model: selectedModel || undefined,
                       is_preorder: isPreorder,
                     })
